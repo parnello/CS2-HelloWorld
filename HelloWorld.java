5c3d638eacdddfb71dc4051e34470cf81dedef4f
parnello <aparnello97@gmail.com>
2015-05-06 08:55:07 -0700

    Added scanner for user input

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class HelloWorld {
+
+  private static Scanner in = new Scanner(System.in);
+
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    System.out.println("Goodbye");
+    System.out.println("What's your name?");
+    String name = in.nextLine();
+    System.out.println("Goodbye, "+name+"!");
+
   }
 }
